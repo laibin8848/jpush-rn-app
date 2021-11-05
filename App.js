769815f8bf1aadd21ea37e274767ec38b88893cb
laibin5 <laibin5@gdii-yueyun.com>
2021-11-05 16:50:30 +0800

    add jpush configs

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
+import JPush from 'jpush-react-native'
 
 export default function App() {
+
+  const notificationListener = result => {
+    const data = {
+        messageID: result.messageID,
+        message: result.content
+    }
+    console.log('data', data)
+}
+
+  useEffect(()=> {
+    JPush.init()
+    JPush.addNotificationListener(notificationListener)
+    JPush.setAlias({sequence: 6,alias:'1111'})
+  }, [])
+
   return (
     <Text>xxx</Text>
   )
